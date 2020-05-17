3325f46f90673e854cde63b2a1e5cfccbba23025
Carlos Meneses Stevens <marsonauta@users.noreply.github.com>
2020-05-16 22:32:48 -0400

    Update script.js
    Setup de masonry con filtros para latamers

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// init Isotope
+// Masonry / Isotope
 var $grid = $('.grid').isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '[data-category]',
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
+  itemSelector: '.latamer',
+  layoutMode: 'fitRows'
 });
-
-// filter functions
 var filterFns = {
   // show if number is greater than 50
   numberGreaterThan50: function() {
@@ -27,26 +15,11 @@ var filterFns = {
     return name.match( /ium$/ );
   }
 };
-
-// bind filter button click
-$('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
+// bind filter on select change
+$('.filters-select').on( 'change', function() {
+  // get filter value from option value
+  var filterValue = this.value;
   // use filterFn if matches value
   filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
-});
-
-// bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-  var sortByValue = $(this).attr('data-sort-by');
-  $grid.isotope({ sortBy: sortByValue });
-});
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
 });
